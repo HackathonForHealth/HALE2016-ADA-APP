a3c96a1343af9a82a8a6c637dd3aeeea3e338bad
Vitor Andrioli <vitor@mandelbrot.com.br>
2016-09-25 15:31:55 -0300

    Sessions page

--- AsmaApp/www/question/question.js
+++ AsmaApp/www/question/question.js
@@ -18,7 +18,39 @@ angular.module('AsmaApp.Question', ['ionic'])
 
 /**************************************** SERVICES ******************************************************/
 
-/**************************************** Controllers ******************************************************/
+.factory('QuestionsService', function($http) {
+
+  // Defines and prefix for the assets url in Android.
+  var url = "";
+  if(ionic.Platform.isAndroid()){
+    url = "/android_asset/www/";
+  }
+
+
+  var question = [];
+  return {
+    getList: function() {
+      // Reads external JSON to get list of supermarkets.
+      return $http.get(url + "json/supermarkets.json").then(function(response){
+        // Returns list of Supermarket objects.
+        supermarkets = response.data;
+        return supermarkets;
+      });
+    },
+    find: function(id) {
+      // Finds one supermarket by id.
+      for(i=0;i<supermarkets.length;i++){
+        if(supermarkets[i].id == parseInt(id)){
+          return supermarkets[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+
+/**************************************** CONTROLLERS ******************************************************/
 
 .controller('QuestionCtrl', function($scope) {
 });
